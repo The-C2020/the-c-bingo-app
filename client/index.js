510f08919725d744294c2bae76eb07c07034bee5
The-C2020 <chris11235@hotmail.com>
2023-05-15 14:56:44 +0200

    Added special online functionality like mark/unmark | Only unmark correct color

--- client/index.js
+++ client/index.js
@@ -1,33 +1,49 @@
 const tiles = document.querySelectorAll(".bingo-tile");
 const color = document.querySelector("#color-picker");
 const select = document.querySelector(".selector");
+const reset = document.querySelector(".resetBoard");
 
 let HOST = location.origin.replace(/^http/, "ws");
 let server = new WebSocket(HOST);
 
 tiles.forEach((tile) => {
-	tile.addEventListener("click", () => {
-		const data = { tileID: tile.id, color: color.value };
-		server.send(JSON.stringify(data));
-	});
+  tile.addEventListener("click", () => {
+    const data = { tileID: tile.id, color: color.value };
+    server.send(JSON.stringify(data));
+  });
+  tile.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    tile.classList.toggle("tile-marked");
+  });
 });
 
-function changeColor() {
-	select.classList.remove("red");
-	select.classList.remove("blue");
-	select.classList.remove("yellow");
-	select.classList.remove("green");
-	select.classList.toggle(select.value);
+reset.addEventListener("click", () => {
+  server.send("reset");
+});
+
+function updateBoard(board) {
+  tiles.forEach((tile, index) => {
+    tile.classList.remove("red");
+    tile.classList.remove("blue");
+    tile.classList.remove("yellow");
+    tile.classList.remove("green");
+    if (board[index]) tile.classList.add(`${board[index]}`);
+  });
 }
 
-//const server = new WebSocket("ws://localhost:3000/websocket");
+function changeColor() {
+  select.classList.remove("red");
+  select.classList.remove("blue");
+  select.classList.remove("yellow");
+  select.classList.remove("green");
+  select.classList.toggle(select.value);
+}
 
 server.onopen = function () {
-	//button.disabled = false;
+  //button.disabled = false;
 };
 
 server.onmessage = function (event) {
-	const data = JSON.parse(event.data);
-	const clickedTile = document.getElementById(data.tileID);
-	clickedTile.classList.toggle(`${data.color}`);
+  const input = JSON.parse(event.data);
+  updateBoard(input);
 };
--- server.js
+++ server.js
@@ -9,22 +9,31 @@ const INDEX = "/index.html";
 app.use(express.static("client"));
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+const board = new Array(25);
+
+function resetBoard() {
+  board.forEach((element, i) => (board[i] = undefined));
+}
 
 wss.on("connection", function (ws) {
-	console.log("Client connected");
-	ws.on("message", function (data) {
-		const info = JSON.parse(data);
-		console.log(info.tileID);
-		console.log(info.color);
-		wss.clients.forEach(function each(client) {
-			if (client.readyState === WebSocket.OPEN) {
-				client.send(JSON.stringify({ tileID: info.tileID, color: info.color }));
-			}
-		});
-	});
-	ws.on("close", () => console.log("Client disconnected"));
+  console.log("Client connected");
+  ws.send(JSON.stringify(board)); // send board to client
+  ws.on("message", function (data) {
+    if (data.toString("utf8") === "reset") resetBoard(); // reset
+    else {
+      const info = JSON.parse(data); // analyze data
+      if (board[info.tileID - 1] === undefined)
+        board[info.tileID - 1] = info.color; // only unoccupied tiles can be marked
+      else if (board[info.tileID - 1] === info.color) board[info.tileID - 1] = undefined; // only the color, which has already marked a tile, can unmark that tile
+    }
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(board));
+      }
+    });
+  });
+  ws.on("close", () => console.log("Client disconnected"));
 });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
